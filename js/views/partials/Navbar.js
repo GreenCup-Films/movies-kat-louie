346ab957801d9fe0bf52fd34b7422d0c1d203b56
Louie Espinosa <louie.espinosa@proton.me>
2022-07-27 14:59:39 -0500

    added some styles, fonts, and a home icon to about screen

--- js/views/partials/Navbar.js
+++ js/views/partials/Navbar.js
@@ -3,7 +3,7 @@ export default function Navbar(props) {
     let html = `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#" data-link>GCF Home</a>
+    <a class="navbar-brand" href="/" data-link>GCF Home</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
